bd3e83d5e1fdd0da3f99e923f401290db5dc07ec
AishwaryaGuledgudda <118333471+AishwaryaGuledgudda@users.noreply.github.com>
2022-11-18 16:50:09 -0500

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -3,20 +3,32 @@ package src;
 import java.util.Scanner;
 
 public class Welcome {
-
+	 Scanner S = new Scanner(System.in) ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the world of Java");
 		Welcome W = new Welcome();
 		System.out.println("The Sum of two numbers are " +W.sum());
+		W.Swap();
 
 	}
  public int sum() {
-	 System.out.println("Enetr the two numbers");
+	 System.out.println("Enter the two numbers");
 	 Scanner S = new Scanner(System.in) ;
 	 int a = S.nextInt();
 	 int b = S.nextInt();
 	 int c = a+b;
 	 return c;
  }
+ 
+ public void Swap()
+ {
+	System.out.println("Enter the nmbers to be swapped");
+	int A = S.nextInt();
+	int B = S.nextInt();
+	A = A+B; 
+	B = A-B; 
+	A = A-B; 
+	System.out.println("Values after swapping are" +A +B);
+}
 }
