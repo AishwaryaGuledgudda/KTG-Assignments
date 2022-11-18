f64cbcafd163c7b7504b0b7249983b30dbad90fa
AishwaryaGuledgudda <118333471+AishwaryaGuledgudda@users.noreply.github.com>
2022-11-18 16:30:01 -0500

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -1,11 +1,22 @@
 package src;
 
+import java.util.Scanner;
+
 public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the world of Java");
+		Welcome W = new Welcome();
+		System.out.println("The Sum of two numbers are " +W.sum());
 
 	}
-
+ public int sum() {
+	 System.out.println("Enetr the two numbers");
+	 Scanner S = new Scanner(System.in) ;
+	 int a = S.nextInt();
+	 int b = S.nextInt();
+	 int c = a+b;
+	 return c;
+ }
 }
