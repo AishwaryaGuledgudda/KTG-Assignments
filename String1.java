9718a1362e1929259ecadee48a4205845c3d3b7d
AishwaryaGuledgudda <118333471+AishwaryaGuledgudda@users.noreply.github.com>
2022-11-22 14:26:48 -0500

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		int i,index;
+		System.out.println("Enter the string");
+		//Scanner str = new Scanner(System.in);
+		String S = "string exercise";
+		
+		System.out.println("The indexe of the entered string is" +S.indexOf("r"));
+			
+		
+
+	}
+
+}
